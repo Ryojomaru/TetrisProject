681b33077dfee3541569d0b4ae7a4efc4d46c608
Ryojomaru <ryojomaru@gmail.com>
2017-02-21 18:41:53 +0100

    Basic menu GUI

---
+++ src/fr/imie/tetrisproject/launcher/Launcher.java
@@ -0,0 +1,13 @@
+package fr.imie.tetrisproject.launcher;
+
+import fr.imie.tetrisproject.gui.Menu;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		
+		Menu pasMenu = new Menu();
+		
+	}
+
+}
